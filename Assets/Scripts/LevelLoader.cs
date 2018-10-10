822c6180c52305c86afca7be40b79e032766258a
Eduardo Wagener <eduardowagener@gmail.com>
2018-10-10 19:36:57 +0200

    Generator

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+	public Slider slider;
+
+	private void Start()
+	{
+		StartCoroutine("LoadAsyncronously", 1);
+	}
+	IEnumerator LoadAsyncronously(int sceneIndex)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+		while (!operation.isDone)
+		{
+			float progress = Mathf.Clamp01(operation.progress / 0.9f);
+			slider.value = progress;
+
+			yield return null;
+		}
+	}
+}
