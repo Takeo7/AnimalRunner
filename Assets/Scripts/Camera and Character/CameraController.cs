d0666f3368ecee9856ddd0b4a001886faa517482
Eduardo Wagener <eduardowagener@gmail.com>
2018-10-17 12:47:16 +0200

    Fixed camera script and scaled character

--- Assets/Scripts/Camera and Character/CameraController.cs
+++ Assets/Scripts/Camera and Character/CameraController.cs
@@ -11,13 +11,14 @@ public class CameraController : MonoBehaviour {
 	public float yOffset;
 	public Camera cam;
 
-	private void LateUpdate()
+	private void Update()
 	{
 		if (target)
 		{
 			Vector3 point = cam.WorldToViewportPoint(target.position);
 			Vector3 delta = target.position - cam.ViewportToWorldPoint(new Vector3(xOffset, yOffset, point.z)); //(new Vector3(0.5, 0.5, point.z));
 			Vector3 destination = transform.position + delta;
+			destination.y = 0;
 			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
 		}
 	}
