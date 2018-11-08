1359cf3b2a52558188a670cb29f7e43c43195928
Eduardo Wagener <eduardowagener@gmail.com>
2018-11-09 00:04:06 +0100

    Fixed Prefabs of Environment Parallax Added

---
+++ Assets/ParallaxController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour {
+
+	public float backgroundSize;
+	public float parallaxSpeed;
+
+	private Transform cameraTransform;
+	private Transform[] layers;
+	private float viewZone = 10;
+	private int leftIndex;
+	private int rightIndex;
+	private float lastCameraX;
+	public float yOffset;
+	public float zOffset;
+
+	public bool isParallax;
+
+	void Start()
+	{
+		cameraTransform = Camera.main.transform;
+		lastCameraX = cameraTransform.position.x;
+		layers = new Transform[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			layers[i] = transform.GetChild(i);
+		}
+		leftIndex = 0;
+		rightIndex = layers.Length - 1;
+	}
+	void Update()
+	{
+		if (isParallax)
+		{
+			float deltaX = cameraTransform.position.x - lastCameraX;
+			transform.position += Vector3.right * (deltaX * parallaxSpeed);
+			lastCameraX = cameraTransform.position.x;
+		}
+		if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
+		{
+			ScrollRight();
+		}
+	}
+	void ScrollRight()
+	{
+		Debug.Log("scrolledRight");
+		int lastLeft = leftIndex;
+		layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize) + new Vector3(0, yOffset,zOffset);
+		rightIndex = leftIndex;
+		leftIndex++;
+		if(leftIndex == layers.Length)
+		{
+			leftIndex = 0;
+		}
+	}
+}
