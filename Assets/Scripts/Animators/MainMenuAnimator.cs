1da98ee2a56320bf163f61192b48a9aefb1b958d
Arturo Wagener <arthur_wag@hotmail.com>
2018-11-19 20:38:08 +0100

    Added UI, Font and Menus

--- Assets/Scripts/Animators/MainMenuAnimator.cs
+++ Assets/Scripts/Animators/MainMenuAnimator.cs
@@ -6,13 +6,59 @@ public class MainMenuAnimator : MonoBehaviour {
 
 	public Animator animator;
 
+    public GameObject nonTouch;
+    public GameObject settingsWindow;
+    public GameObject shopWindow;
+    public GameObject IntroWindow;
+
+
+    private void Start()
+    {
+        ToogleIntro();
+    }
+
+
+    public void ToogleSettingsWindow()
+    {
+        settingsWindow.SetActive(!settingsWindow.active);
+    }
+    public void ToogleShopWindow()
+    {
+        shopWindow.SetActive(!shopWindow.active);
+    }
+    public void ToogleNonTouch(bool b)
+    {
+        nonTouch.SetActive(b);
+    }
 	public void ToggleOff()
 	{
 		animator.SetTrigger("ToggleOff");
 	}
-
+    public void ToogleIntro()
+    {
+        StartCoroutine("IntroCoroutine");   
+    }
 	public void StartGame()
 	{
+        ToggleOff();
+        ToogleNonTouch(true);
+        if (settingsWindow.active == true)
+        {
+            ToogleSettingsWindow();
+        }
+        if (shopWindow.active == true)
+        {
+            ToogleShopWindow();
+        }     
 		EnvironmentController.instance.StartGame();
 	}
+
+    IEnumerator IntroCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        ToogleNonTouch(false);
+        IntroWindow.SetActive(false);
+        StopCoroutine("IntroCoroutine");
+    }
+
 }
