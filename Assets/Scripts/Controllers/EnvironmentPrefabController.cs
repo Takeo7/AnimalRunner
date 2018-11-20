ea3397c6a2b70a8d33dceb0b43bc39b06bddfe23
Arturo Wagener <arthur_wag@hotmail.com>
2018-11-20 23:21:55 +0100

    Adjusted Enviroments Decorations Procedural adjusted Enemy & traps generation adjusted Created Bear Trap Created bool isEnemy&isTrap spawneable

--- Assets/Scripts/Controllers/EnvironmentPrefabController.cs
+++ Assets/Scripts/Controllers/EnvironmentPrefabController.cs
@@ -7,60 +7,88 @@ public class EnvironmentPrefabController : MonoBehaviour {
 	public SpriteRenderer floorRenderer;
 	EnvironmentController EC;
 	public BoxCollider2D collider;
-    public Transform[] SpawnPoints;
+    public Transform[] enemySpawnPoints;
+    public Transform[] trapSpawnPoints;
+    public Transform[] decorationSpawnPoints;
     public GameObject[] enemys;
     public GameObject[] traps;
     public GameObject[] decorations;
-    public bool isSpawneable;
+    public bool isEnemySpawneable;
+    public bool isTrapSpawneable;
+    public int numberOfDeco = 2;
 
-	private void Start()
+    private void Start()
 	{
 		EC = EnvironmentController.instance;
-        int rand = Random.Range(0, SpawnPoints.Length - 1);
-        int rand2 = Random.Range(0, SpawnPoints.Length - 1);
-        int rand3 = Random.Range(0, SpawnPoints.Length - 1);
-        if (rand == rand2)
-        {
-            rand2 = Random.Range(0, SpawnPoints.Length - 1);
 
+        if (isEnemySpawneable)
+        {
+            float isEnemy = Random.Range(0, 2);
+            if (isEnemy <= 1)
+            {
+                CreateNewEnemy();
+            }
         }
-        if (isSpawneable)
+        if (isTrapSpawneable)
         {
-            int randTrap = Random.Range(0, traps.Length);
-            if (randTrap == traps.Length)
+            float isTrap = Random.Range(0, 2);
+            if (isTrap <= 1)
             {
-                randTrap = 0;
+                CreateNewTrap();
             }
+        }
+        try
+        {
+            CreateNewDeco(numberOfDeco);
+        }
+        catch (System.Exception)
+        {
 
-            int randEnemy = Random.Range(0, enemys.Length);
-            if (randEnemy == enemys.Length)
+            throw;
+        } 
+
+    }
+
+    void CreateNewEnemy()
+    {
+        int rand = Random.Range(0, enemySpawnPoints.Length - 1);
+        int randEnemy = Random.Range(0, enemys.Length);
+        if (randEnemy == enemys.Length)
+        {
+            randEnemy = 0;
+        }
+        GameObject g = Instantiate(enemys[randEnemy], enemySpawnPoints[rand].position, Quaternion.identity);
+        g.transform.SetParent(enemySpawnPoints[rand]);
+    }
+    void CreateNewTrap()
+    {
+        int rand2 = Random.Range(0, trapSpawnPoints.Length - 1);
+        int randTrap = Random.Range(0, traps.Length);
+        if (randTrap == traps.Length)
+        {
+            randTrap = 0;
+        }
+
+        GameObject j = Instantiate(traps[randTrap], trapSpawnPoints[rand2].position, Quaternion.identity);
+        j.transform.SetParent(trapSpawnPoints[rand2]);
+    }
+    void CreateNewDeco(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            int rand3 = Random.Range(0, decorationSpawnPoints.Length);
+            if (rand3 == decorationSpawnPoints.Length)
             {
-                randEnemy = 0;
+                rand3 = 0;
             }
-
             int randDeco = Random.Range(0, decorations.Length);
             if (randDeco == decorations.Length)
             {
                 randDeco = 0;
             }
-
-            float isEnemy = Random.Range(0, 4);
-            float isTrap = Random.Range(0, 4);
-
-            if (isEnemy <= 1)
-            {
-                GameObject g = Instantiate(enemys[randEnemy], SpawnPoints[rand].position, Quaternion.identity);
-                g.transform.SetParent(SpawnPoints[rand]);
-            }
-            if (isTrap <= 1)
-            {
-                GameObject j = Instantiate(traps[randTrap], SpawnPoints[rand2].position, Quaternion.identity);
-                j.transform.SetParent(SpawnPoints[rand2]);
-            }
-            GameObject deco = Instantiate(decorations[randDeco], new Vector3(SpawnPoints[rand3].position.x, SpawnPoints[rand3].position.y, 1), Quaternion.identity);
-            deco.transform.SetParent(SpawnPoints[rand3]);
-
+            GameObject deco = Instantiate(decorations[randDeco], new Vector3(decorationSpawnPoints[rand3].position.x, decorationSpawnPoints[rand3].position.y, 1), Quaternion.identity);
+            deco.transform.SetParent(decorationSpawnPoints[rand3]);
         }
-
-	}
+        
+    }
 }
