628765b8129fd08edfbdd7b83fd40fa433791e47
Arturo Wagener <arthur_wag@hotmail.com>
2018-12-02 20:57:03 +0100

    Starting Languajes

---
+++ Assets/Scripts/LanguajesDic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class LanguajesDic : ScriptableObject {
+
+    Languajes currentlang;
+
+    Dictionary<int, string> Dic = new Dictionary<int, string>();
+    
+    public string GetText(int key)
+    {
+        return Dic[key];
+    }
+
+    public enum Languajes
+    {
+        English,
+        Spanish,
+        French,
+        Chiniese
+    }
+}
