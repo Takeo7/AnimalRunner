bdc8665650eff839e21b2c53106b4ad8c90bdaf9
Takeo7 <32916919+Takeo7@users.noreply.github.com>
2019-03-08 01:41:11 +0100

    Jump
    Jump updated to not interact with UI in touch

--- Assets/JumpControl.cs
+++ Assets/JumpControl.cs
@@ -12,11 +12,20 @@ public class JumpControl : MonoBehaviour {
 	{
 		if (EC.inGame)
 		{
-			if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+			if (Input.GetMouseButtonDown(0) && IsPointerOverUIObject() == false)
 			{
 				CR.Jump();
 				//Debug.Log("Jump");
 			}
 		}
 	}
+
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
 }
